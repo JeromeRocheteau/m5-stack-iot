f90739fedce15091931fafdaf4e554e663e475c3
Jérôme Rocheteau <jerome.rocheteau@icam.fr>
2021-02-05 10:11:21 +0100

    add example of light unit with m5 atom lite

---
+++ examples/m5_atom/m5_atom_lite/m5_atom_lite_02_light_unit/m5_atom_lite_02_light_unit.ino
@@ -0,0 +1,20 @@
+#include <M5Atom.h>
+
+uint16_t value;
+
+void setup() {
+  M5.begin(true, false, true);
+  Serial.begin(115200);
+  pinMode(36, INPUT);
+  pinMode(26, INPUT);
+  value = 0;
+}
+
+void loop() {
+  value = value = analogRead(36);
+  Serial.println(value);
+  delay(50);
+  value = digitalRead(26);
+  Serial.println(value);
+  delay(200);
+}
